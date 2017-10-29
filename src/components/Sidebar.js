4ebf0c8c2f34895f328b9b8c35ecc11b666fca56
Alex Sánchez <alxsnchez@gmail.com>
2017-10-29 23:43:53 +0100

    refactor: Sidebar component to stateless

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,40 +2,37 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-class Sidebar extends Component {
-    render() {
-        return(
-            <div>
-                <button onClick={this.props.open}>ADD NEW POST</button>
-                <p/>
-                {this.renderCategories()}
-            </div>
-        )
-
+const Sidebar = (props) => {
+    const renderCategories = () => {
+        const { categories } = props
+    
+        if(categories.length) {
+            const categoriesLinks = categories.map( category => {
+                return(
+                  <Link key={category.path} to={`/${category.path}`} className='link'>
+                  <div className='category'>
+                    <div className='category-logo'>
+                      <img src={require(`../img/${category.path}.png`)} alt={category.name}/>
+                    </div>
+                    <div className='category-name'>
+                      {category.name}
+                    </div>
+                  </div>
+                  </Link>
+                  )
+              })
+        
+              return categoriesLinks
+        }
     }
 
-    renderCategories = () => {
-        const { categories } = this.props
-
-        if(categories.length) {
-          const categoriesLinks = categories.map( category => {
-            return(
-              <Link key={category.path} to={`/${category.path}`} className='link'>
-              <div className='category'>
-                <div className='category-logo'>
-                  <img src={require(`../img/${category.path}.png`)} alt={category.name}/>
-                </div>
-                <div className='category-name'>
-                  {category.name}
-                </div>
-              </div>
-              </Link>
-              )
-          })
-    
-          return categoriesLinks
-        }
-      }
+    return (
+        <div>
+            <button onClick={props.open}>ADD NEW POST</button>
+            <p/>
+            {renderCategories()}
+        </div>
+    )
 }
 
 const mapStateToProps = (state, props) => ({
